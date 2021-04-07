1bf52946039a21345c741f491779e331b300b708
ekkun <eiji@ekkun.com>
2021-04-07 10:00:38 +0900

    move: ディレクトリ変更

---
+++ src/js/app/include.js
@@ -0,0 +1,13 @@
+/**
+ * ekkun.com
+ * @version 0.1.0
+ * @author eiji@ekkun.com
+ * @license -
+ */
+window.addEventListener('load', () => {
+  include();
+});
+
+const include = () => {
+  console.log('インクルードしちゃったんです！');
+};
